60a8850bd45464ac218ea41f02f81f719a03702f
Jason Emanuel S <jemanuel@student.ciputra.ac.id>
2024-12-09 15:04:27 +0700

    Initial Commit

---
+++ UserInterviewDemo/UserInterviewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterviewDemoApp.swift
+//  UserInterviewDemo
+//
+//  Created by Jason Emanuel on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UserInterviewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserInterviewDemoTests/UserInterviewDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterviewDemoTests.swift
+//  UserInterviewDemoTests
+//
+//  Created by Jason Emanuel on 09/12/24.
+//
+
+import Testing
+@testable import UserInterviewDemo
+
+struct UserInterviewDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
